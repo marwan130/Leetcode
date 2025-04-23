60c260a518795094a41a95894af6e0447245be44
marwan130 <marwanhossam130@gmail.com>
2025-04-23 21:25:08 +0200

    [1] [Time Beats: 77.64%] [Memory Beats: 36.25%] - LeetPush

---
+++ Two-Sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map <int, int> map;
+        for(int i=0; i<nums.size(); i++) {
+            int look_for = target - nums[i];
+            auto it = map.find(look_for);
+            if(it != map.end()) {
+                return {it->second, i};
+            }
+            map.insert({nums[i], i});
+        }
+        return {};
+    }
+};
