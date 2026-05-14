d2c37781865c841f2a0b6a1300392aa1b2e4d88a
marwan130 <marwanhossam130@gmail.com>
2026-05-15 02:44:06 +0300

    0121-BEST-TIME-TO-BUY-AND-SELL-STOCK Stats: Time: 2 ms (31.42%), Space: 97.5 MB (6.82%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        if(prices.size() == 1) {
+        if(prices.size() <= 1) {
             return 0;
         }
-
         int maxProfit = 0, buy = INT_MAX;
 
         for(int price : prices) {
             if(price < buy) {
                 buy = price;
             }
-            else {
-                maxProfit = max(maxProfit, price - buy);
-            }
+
+            maxProfit = max(maxProfit, price - buy);
         }
         return maxProfit;
     }
