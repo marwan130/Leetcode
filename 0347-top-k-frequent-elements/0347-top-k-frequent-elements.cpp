2b10c907aeb4385c1d2434f1f58a5442553bb3b0
marwan130 <marwanhossam130@gmail.com>
2026-05-15 01:48:33 +0300

    0347-TOP-K-FREQUENT-ELEMENTS Stats: Time: 0 ms (100%), Space: 19.8 MB (20.68%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> map;
+        unordered_map<int, int> freq;
+        int most = 0;
+        vector<int> res;
 
         for(int num : nums) {
-            map[num]++;
+            freq[num]++;
         }
 
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minHeap;
+        vector<vector<int>> bucket(nums.size() + 1);
 
-        for(auto& [num, freq] : map) {
-            minHeap.emplace(freq, num);
-            if(minHeap.size() > k) {
-                minHeap.pop();
+        for(const auto& pair : freq) {
+            bucket[pair.second].push_back(pair.first);
+        }
+
+        for(int i = nums.size(); i >= 0; i--) {
+            for(int num : bucket[i]) {
+                res.push_back(num);
+            }
+
+            if(res.size() == k) {
+                return res;
             }
         }
-        vector<int> res;
-        while(!minHeap.empty()) {
-            res.push_back(minHeap.top().second);
-            minHeap.pop();
-        }
-
         return res;
     }
 };
